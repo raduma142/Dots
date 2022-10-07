fd832e3e07bafad26626f807de824e004c461673
Дмитрий <@R-PC>
2022-10-08 02:45:06 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Dots.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dots.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        //Properties
+
+        #region Message
+        private string _Message = "Dots!";
+
+        public string Message
+        {
+            get => _Message;
+            set => Set(ref _Message, value);
+        }
+        #endregion
+
+
+    }
+}
